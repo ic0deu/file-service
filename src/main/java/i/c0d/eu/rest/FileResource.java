ec9dd9b86111ba415087f4cfb1dee5b11c2ca6fb
amasucci <antmas83@gmail.com>
2016-06-01 00:02:32 +0200

    Adding first test

--- src/main/java/i/c0d/eu/rest/FileResource.java
+++ src/main/java/i/c0d/eu/rest/FileResource.java
@@ -1,5 +1,7 @@
 package i.c0d.eu.rest;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import i.c0d.eu.resource.FileMetadata;
 import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
@@ -14,10 +16,8 @@ import javax.ws.rs.core.Response;
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by antonio on 18/05/2016.
@@ -37,34 +37,40 @@ public class FileResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response upload(@PathParam("fileId") String fileId,
                            @FormDataParam("fileDescription") FormDataBodyPart jsonPart,
-                           @FormDataParam("file") List<FormDataBodyPart> parts) {
+                           @FormDataParam("file") List<FormDataBodyPart> parts) throws IOException {
         if (jsonPart != null) {
             jsonPart.setMediaType(MediaType.APPLICATION_JSON_TYPE);
             FileMetadata[] fileMetadata = jsonPart.getValueAs(FileMetadata[].class);
             Arrays.stream(fileMetadata).forEach( e -> System.out.println(e.toString()));
         }
+        List<String> fileNames = new ArrayList<>();
         for (FormDataBodyPart part : parts) {
             FormDataContentDisposition disp = part.getFormDataContentDisposition();
             InputStream in = part.getValueAs(InputStream.class);
+            String fileName = new StringBuilder().append(fileId).append(new Date().getTime()).append(disp.getFileName()).toString().trim();
 
             try {
-                int read = 0;
+                int read;
                 byte[] bytes = new byte[1024];
 
-                OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + fileId + new Date().getTime() + disp.getFileName()));
+                OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + fileName));
                 while ((read = in.read(bytes)) != -1) {
                     out.write(bytes, 0, read);
                 }
                 out.flush();
                 out.close();
+                fileNames.add(fileName);
             } catch (IOException e) {
                 throw new WebApplicationException("Error while uploading file. Please try again !!");
             }
 
         }
 
-
-        return Response.ok("Data uploaded successfully !!").build();
+        String response = new StringBuilder().append("{'result': 'Data Uploaded Successfully', 'fileNames': ['")
+                .append(fileNames.stream().map(s -> s).collect(Collectors.joining("','")))
+                .append("']}").toString();
+        JsonNode jsonNode = new ObjectMapper().readTree(response.replace("'","\""));
+        return Response.ok(jsonNode).build();
     }
 
 }
